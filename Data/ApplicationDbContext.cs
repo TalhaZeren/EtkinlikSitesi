f1567524fd138f85f8a1763f3f68064b44c514f3
foreign <foreign@gmail.com>
2024-07-29 09:47:16 +0300

    Veritabanina baglanildi.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EtkinlikProjesi.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace EtkinlikProjesi.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Connect to postgres with connection String from App Setting.
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+        }
+        public DbSet<User> User { get; set; }
+
+    }
+}
